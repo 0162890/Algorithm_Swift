f6a5828a332f630b71c4088d868fa33501282df3
hayeon.kim <hayeon.kim@navercorp.com>
2020-09-16 14:09:45 +0900

    runningSum

--- Leetcode/runningSum.swift
+++ Leetcode/runningSum.swift
@@ -1,6 +1,9 @@
-import UIKit
-
-// https://leetcode.com/problems/running-sum-of-1d-array/
+//
+//  runningSum.swift
+//
+//  https://leetcode.com/problems/running-sum-of-1d-array/
+//  Created by 하연 on 2020/09/16.
+//
 
 func runningSum(_ nums: [Int]) -> [Int] {
     var newNums = [Int]()
@@ -13,3 +16,4 @@ func runningSum(_ nums: [Int]) -> [Int] {
     
     return newNums
 }
+
